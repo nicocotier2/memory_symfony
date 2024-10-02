ed811ef3231ba28ea48d969778d4bbc72ee0db68
nicocotier2 <nicolas.tenneguin@gmail.com>
2024-10-02 14:46:15 +0200

    creation of command that create theme and card

---
+++ migrations/Version20241002111254.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241002111254 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE carte (id INT AUTO_INCREMENT NOT NULL, theme_id INT NOT NULL, type VARCHAR(255) NOT NULL, lien VARCHAR(255) NOT NULL, INDEX IDX_BAD4FFFD59027487 (theme_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE theme (id INT AUTO_INCREMENT NOT NULL, nom_theme VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE carte ADD CONSTRAINT FK_BAD4FFFD59027487 FOREIGN KEY (theme_id) REFERENCES theme (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE carte DROP FOREIGN KEY FK_BAD4FFFD59027487');
+        $this->addSql('DROP TABLE carte');
+        $this->addSql('DROP TABLE theme');
+    }
+}
